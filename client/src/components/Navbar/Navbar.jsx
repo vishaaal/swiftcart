0ba97055f36a63e765e2fd5c6c5bada729b551b9
vishaaal <officialvk02@gmail.com>
2023-08-17 18:29:52 +0530

    [add] icons and img

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import SearchIcon from '@mui/icons-material/Search';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import ShoppingCartCheckoutIcon from '@mui/icons-material/ShoppingCartCheckout';
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="left"></div>
+        <div className="center"></div>
+        <div className="right"></div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
