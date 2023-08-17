5c422786b4489cf0ca807c502908b3e54c67c40d
vishaaal <officialvk02@gmail.com>
2023-08-17 18:01:09 +0530

    add components - Navbar - Footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,24 @@
 import {
   createBrowserRouter,
   RouterProvider,
+  Outlet
 } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Product from "./pages/Product/Product";
+import Products from "./pages/Products/Products";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <span>Home</span>
+    element: <Home/>
   },
   {
     path: "/products/:id",
-    element: <span>Category</span>
+    element: <Products/>
   },
   {
     path: "/product/:id",
-    element: <span>Product</span>
+    element: <Product/>
   },
 ])
 
