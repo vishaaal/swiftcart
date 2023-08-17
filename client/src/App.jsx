aeacfa620287e4a5336a322484c01e62a2b440b9
vishaaal <officialvk02@gmail.com>
2023-08-17 17:56:05 +0530

    add react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,29 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <span>Home</span>
+  },
+  {
+    path: "/products/:id",
+    element: <span>Category</span>
+  },
+  {
+    path: "/product/:id",
+    element: <span>Product</span>
+  },
+])
 
 function App() {
-  return <div>Hello World!</div>;
+  return (
+  <div>
+    <RouterProvider router = {router}/>
+  </div>
+  );
 }
 
 export default App;
