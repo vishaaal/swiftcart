f589477e52c80bce9d50f55dd983a3cefaf4444c
vishaaal <officialvk02@gmail.com>
2023-08-20 12:27:03 +0530

    [add] FeatureProducts page

---
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./FeaturedProucts.scss"
+const FeaturedProducts = () => {
+  return (
+    <div className='featuredProducts'>FeaturedProducts</div>
+  )
+}
+
+export default FeaturedProducts
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,10 +1,15 @@
+import FeaturedProducts from "../../components/FeaturedProducts/FeaturedProducts";
 import Slider from "../../components/Slider/Slider";
 import React from "react";
 
 const Home = () => {
-  return <div className="home">
-    <Slider/>
-  </div>;
+  return (
+    <div className="home">
+      <Slider />
+      <FeaturedProducts type="featured" />
+      <FeaturedProducts type="trending" />
+    </div>
+  );
 };
 
 export default Home;
