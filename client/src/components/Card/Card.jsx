59036b94facc9ae15df78144accc14c026258b6c
vishaaal <officialvk02@gmail.com>
2023-08-20 13:07:04 +0530

    [feat] New Season

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -7,6 +7,7 @@ const Card = ({ item }) => {
     <Link className="link" to={`/product/${item.id}`}>
       <div className="card">
         <div className="image">
+            {item.isNew && <span>New Season</span>}
           <img src={item.img} alt="" className="mainImg" />
           <img src={item.img2} alt="" className="secondImg" />
         </div>
