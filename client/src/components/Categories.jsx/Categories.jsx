4d090960be62f769e0ea1b08ad7198360e5c8ddd
vishaaal <officialvk02@gmail.com>
2023-08-20 13:10:21 +0530

    [add] component - Categories

---
+++ client/src/components/Categories.jsx/Categories.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Categories.scss"
+
+const Categories = () => {
+  return (
+    <div className='categories'>Categories</div>
+  )
+}
+
+export default Categories
