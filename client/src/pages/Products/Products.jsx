327edc4d870c8edf7984c6a9d4fcfe627f0d35d2
vishaaal <officialvk02@gmail.com>
2023-08-20 15:18:56 +0530

    [feat] Products page

--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React from "react";
+import "./Products.scss";
+import List from "../../components/List/List";
+import { useParams } from "react-router-dom";
+import { useState } from "react";
 
 const Products = () => {
-  return (
-    <div>Products</div>
-  )
-}
 
-export default Products
+  const catId = parseInt(useParams().id)
+  const [maxPrice, setMaxPrice] = useState(1000);
+
+  return (
+    <div className="products">
+      <div className="left">
+        <div className="filterItem">
+          <h2>Product Categories</h2>
+          <div className="inputItem">
+            <input type="checkbox" id="1" value={1} />
+            <label htmlFor="1">Shoes</label>
+          </div>
+          <div className="inputItem">
+            <input type="checkbox" id="2" value={2} />
+            <label htmlFor="2">Jeans</label>
+          </div>
+          <div className="inputItem">
+            <input type="checkbox" id="3" value={3} />
+            <label htmlFor="3">Coats</label>
+          </div>
+        </div>
+        <div className="filterItem">
+          <h2>Filter By Price</h2>
+          <div className="inputItem">
+            <span>0</span>
+            <input type="range" min={0} max={1000} onChange={(e)=>setMaxPrice(e.target.value)}/>
+            <span>{maxPrice}</span>
+          </div>
+        </div>
+        <div className="filterItem">
+          <h2>Sort By</h2>
+          <div className="inputItem">
+            <input type="radio" id="asc" value="asc" name="price" />
+            <label htmlFor="asc">Price (Lowest First)</label>
+          </div>
+          <div className="inputItem">
+            <input type="radio" id="desc" value="desc" name="price" />
+            <label htmlFor="desc">Price (Highest First)</label>
+          </div>
+        </div>
+      </div>
+      <div className="right">
+        <img
+          className="catImg"
+          src="https://images.pexels.com/photos/1074535/pexels-photo-1074535.jpeg?auto=compress&cs=tinysrgb&w=1600"
+          alt=""
+        />
+        <List/>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
