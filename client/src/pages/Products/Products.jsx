2da9ba66064298e758811f40b842061d7a3ecfb2
vishaaal <officialvk02@gmail.com>
2023-08-20 15:21:41 +0530

    [feat] sort Prod by prize

--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -8,6 +8,7 @@ const Products = () => {
 
   const catId = parseInt(useParams().id)
   const [maxPrice, setMaxPrice] = useState(1000);
+  const [sort, setSort] = useState(null)
 
   return (
     <div className="products">
@@ -38,11 +39,11 @@ const Products = () => {
         <div className="filterItem">
           <h2>Sort By</h2>
           <div className="inputItem">
-            <input type="radio" id="asc" value="asc" name="price" />
+            <input type="radio" id="asc" value="asc" name="price" onChange={e=>setSort("asc")} />
             <label htmlFor="asc">Price (Lowest First)</label>
           </div>
           <div className="inputItem">
-            <input type="radio" id="desc" value="desc" name="price" />
+            <input type="radio" id="desc" value="desc" name="price" onChange={e=>setSort("desc")} />
             <label htmlFor="desc">Price (Highest First)</label>
           </div>
         </div>
@@ -53,7 +54,7 @@ const Products = () => {
           src="https://images.pexels.com/photos/1074535/pexels-photo-1074535.jpeg?auto=compress&cs=tinysrgb&w=1600"
           alt=""
         />
-        <List/>
+        <List catId={catId} maxPrice={maxPrice} sort={sort}/>
       </div>
     </div>
   );
