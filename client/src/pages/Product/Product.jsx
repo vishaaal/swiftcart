05835854b8bae6905ce33018118cb22449c8c7ed
vishaaal <officialvk02@gmail.com>
2023-08-20 16:24:32 +0530

    [feat] Product.jsx right portion

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -1,27 +1,71 @@
 import React, { useState } from "react";
-import "./Product.scss"
+import "./Product.scss";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import BalanceIcon from "@mui/icons-material/Balance";
 
 const Product = () => {
-  const [selectedImg, setSelectedImg] = useState(0)
+  const [selectedImg, setSelectedImg] = useState(0);
+  const [quantity, setQuantity] = useState(0);
   const images = [
     "https://images.pexels.com/photos/10026491/pexels-photo-10026491.png?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    "https://images.pexels.com/photos/12179283/pexels-photo-12179283.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
-
-  ]
+    "https://images.pexels.com/photos/12179283/pexels-photo-12179283.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+  ];
 
   return (
     <div className="product">
       <div className="left">
         <div className="images">
-          <img src={images[0]} alt="" onClick={e=>setSelectedImg(0)}/>
-          <img src={images[1]} alt="" onClick={e=>setSelectedImg(1)}/>
-
+          <img src={images[0]} alt="" onClick={(e) => setSelectedImg(0)} />
+          <img src={images[1]} alt="" onClick={(e) => setSelectedImg(1)} />
         </div>
         <div className="mainImg">
-          <img src={images[selectedImg]} alt=""/>
+          <img src={images[selectedImg]} alt="" />
         </div>
       </div>
-      <div className="right"></div>
+      <div className="right">
+        <h1>Title</h1>
+        <span>₹199</span>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates
+          laboriosam voluptatibus, deleniti velit maiores, sequi repellendus in
+          odit dolores tenetur cupiditate ullam ratione doloremque quae expedita
+          exercitationem quas commodi
+        </p>
+        <div className="quantity">
+          <button
+            onClick={() => setQuantity((prev) => (prev === 0 ? 0 : prev - 1))}
+          >
+            -
+          </button>
+          {quantity}
+          <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
+        </div>
+        <button className="add">
+          <AddShoppingCartIcon /> ADD TO CART
+        </button>
+        <div className="links">
+              <div className="item">
+                <FavoriteBorderIcon /> ADD TO WISH LIST
+              </div>
+              <div className="item">
+                <BalanceIcon /> ADD TO COMPARE
+              </div>
+            </div>
+            <div className="info">
+              <span>Vendor: Polo</span>
+              <span>Product Type: T-Shirt</span>
+              <span>Tag: T-Shirt, Women, Top</span>
+            </div>
+            <hr />
+            <div className="info">
+              <span>DESCRIPTION</span>
+              <hr />
+              <span>ADDITIONAL INFORMATION</span>
+              <hr />
+              <span>FAQ</span>
+            </div>
+          </div>
     </div>
   );
 };
