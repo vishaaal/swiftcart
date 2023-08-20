aa02bcf1ee0211d3eea7cc531442ccf67c238754
vishaaal <officialvk02@gmail.com>
2023-08-20 13:14:30 +0530

    [feat] Categories.jsx w/ import

--- client/src/components/Categories.jsx/Categories.jsx
+++ client/src/components/Categories.jsx/Categories.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import "./Categories.scss"
+import React from "react";
+import "./Categories.scss";
 
 const Categories = () => {
   return (
-    <div className='categories'>Categories</div>
-  )
-}
+    <div className="categories">
+      <div className="col">
+        <div className="row">r1</div>
+        <div className="row">r2</div>
+      </div>
+      <div className="col">
+        <div className="row">r3</div>
+      </div>
+      <div className="col-l"></div>
+      <div className="row">
+        <div className="col">
+            <div className="row">r4</div>
+        </div>
+        <div className="col">
+            <div className="row">r5</div>
+        </div>
+      </div>
+      <div className="row">r6</div>
 
-export default Categories
+    </div>
+  );
+};
+
+export default Categories;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import Categories from "../../components/Categories.jsx/Categories";
 import FeaturedProducts from "../../components/FeaturedProducts/FeaturedProducts";
 import Slider from "../../components/Slider/Slider";
 import React from "react";
@@ -7,6 +8,7 @@ const Home = () => {
     <div className="home">
       <Slider />
       <FeaturedProducts type="featured" />
+      <Categories/>
       <FeaturedProducts type="trending" />
     </div>
   );
