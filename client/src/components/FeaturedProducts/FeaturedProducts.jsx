a8638732c4c8856a3697dd6caae74a2a3628f4c8
vishaaal <officialvk02@gmail.com>
2023-08-20 12:41:32 +0530

    [add] data to FeaturedProducts

--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
-import "./FeaturedProucts.scss"
-const FeaturedProducts = () => {
-  return (
-    <div className='featuredProducts'>FeaturedProducts</div>
-  )
-}
+import React from "react";
+import "./FeaturedProucts.scss";
 
-export default FeaturedProducts
+const FeaturedProducts = ({ type }) => {
+  const data = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos.1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      img2: "https://images.pexels.com/photos.1163914/pexels-photo-1163914.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      title: "Long Sleeve Graphic T-shirt",
+      isNew: true,
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos.1759622/pexels-photo-1759622.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      title: "Coat",
+      isNew: true,
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos.1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      title: "Skirt",
+      isNew: true,
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 4,
+      img: "https://images.pexels.com/photos.2065200/pexels-photo-2065200.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      title: "Hat",
+      isNew: true,
+      oldPrice: 19,
+      price: 12,
+    },
+  ];
+
+  return (
+    <div className="featuredProducts">
+      <div className="top">
+        <h1>{type} products</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+          natus quam et asperiores consequatur commodi quidem deleniti corporis
+          accusamus iste aut, culpa, qui quae voluptate eligendi dolorem itaque?
+          Laborum sit, consequuntur tenetur sed porro atque excepturi corrupti
+          natus culpa! Facilis.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedProducts;
