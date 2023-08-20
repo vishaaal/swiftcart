c87eed5bcd3370fe8f7e2bf2ecabb7f9d3053e34
vishaaal <officialvk02@gmail.com>
2023-08-20 13:01:46 +0530

    [style] feat Card

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import "./Card.scss"
+import React from "react";
+import "./Card.scss";
+import { Link } from "react-router-dom";
 
-const Card = () => {
+const Card = ({ item }) => {
   return (
-    <div className='card'>Card</div>
-  )
-}
+    <Link className="link" to={`/product/${item.id}`}>
+      <div className="card">
+        <div className="image">
+          <img src={item.img} alt="" className="mainImg" />
+          <img src={item.img2} alt="" className="secondImg" />
+        </div>
+        <h2>{item.title}</h2>
+        <div className="prices">
+          <h3>${item.oldPrice}</h3>
+          <h3>${item.price}</h3>
+        </div>
+      </div>
+    </Link>
+  );
+};
 
-export default Card
+export default Card;
