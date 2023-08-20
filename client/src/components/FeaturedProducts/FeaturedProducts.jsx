a99c22c439ad365c04ef0f351ba685d07d746293
vishaaal <officialvk02@gmail.com>
2023-08-20 13:05:20 +0530

    [fix] minor issues

--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -7,7 +7,7 @@ const FeaturedProducts = ({ type }) => {
     {
       id: 1,
       img: "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      img2: "https://images.pexels.com/photos/1163914/pexels-photo-1163914.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
       title: "Long Sleeve Graphic T-shirt",
       isNew: true,
       oldPrice: 19,
