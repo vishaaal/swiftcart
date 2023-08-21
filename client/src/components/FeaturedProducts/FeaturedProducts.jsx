08c22e26eb1bbadf3bb772404c5a3c629f1f0af4
vishaaal <officialvk02@gmail.com>
2023-08-21 06:29:31 +0530

    [fetch] data to Card

--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -4,56 +4,22 @@ import Card from "../Card/Card";
 import axios from "axios";
 
 const FeaturedProducts = ({ type }) => {
-  const data = [
-    {
-      id: 1,
-      img: "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      title: "Long Sleeve Graphic T-shirt",
-      isNew: true,
-      oldPrice: 19,
-      price: 12,
-    },
-    {
-      id: 2,
-      img: "https://images.pexels.com/photos/1759622/pexels-photo-1759622.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      title: "Coat",
-      isNew: true,
-      oldPrice: 19,
-      price: 12,
-    },
-    {
-      id: 3,
-      img: "https://images.pexels.com/photos/1457983/pexels-photo-1457983.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      title: "Skirt",
-      isNew: true,
-      oldPrice: 19,
-      price: 12,
-    },
-    {
-      id: 4,
-      img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      title: "Hat",
-      isNew: true,
-      oldPrice: 19,
-      price: 12,
-    },
-  ];
+ 
 
-  const [products, setProducts] = useState([]);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const data = await axios.get(
-          process.env.REACT_APP_API_URL + "/products",
+        const res = await axios.get(
+          process.env.REACT_APP_API_URL + "/products?populate=*",
           {
             headers: {
               Authorization: "bearer " + process.env.REACT_APP_API_TOKEN,
             },
           }
         );
-        console.log(data);
+        setData(res.data.data)
       } catch (err) {
         console.log(err);
       }
