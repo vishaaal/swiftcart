bdbba93e6ee2d8508a807569ae9949b935ba1d2a
vishaaal <officialvk02@gmail.com>
2023-08-21 20:14:18 +0530

    [func] persist cart Data - reduxpersist

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { persistor, store } from "./redux/store";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-
   <React.StrictMode>
     <Provider store={store}>
-    <App/>
+      <PersistGate loading={"loading"} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
