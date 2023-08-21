c8617ebe1a72067c081e9a7ab3073fd20c67ddb5
vishaaal <officialvk02@gmail.com>
2023-08-21 06:43:27 +0530

    [feat] useFetch.js

--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,48 +1,31 @@
-import React, { useEffect, useState } from "react";
-import "./FeaturedProucts.scss";
+import React from "react";
 import Card from "../Card/Card";
-import axios from "axios";
+import "./FeaturedProucts.scss"
+import useFetch from "../../hooks/useFetch";
 
 const FeaturedProducts = ({ type }) => {
- 
-
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await axios.get(
-          process.env.REACT_APP_API_URL + `/products?populate=*&[filters][type][$eq]=${type}`,
-          {
-            headers: {
-              Authorization: "bearer " + process.env.REACT_APP_API_TOKEN,
-            },
-          }
-        );
-        setData(res.data.data)
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    fetchData();
-  }, []);
+  const { data, loading, error } = useFetch(
+    `/products?populate=*&[filters][type][$eq]=${type}`
+  );
 
   return (
     <div className="featuredProducts">
       <div className="top">
         <h1>{type} products</h1>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
-          natus quam et asperiores consequatur commodi quidem deleniti corporis
-          accusamus iste aut, culpa, qui quae voluptate eligendi dolorem itaque?
-          Laborum sit, consequuntur tenetur sed porro atque excepturi corrupti
-          natus culpa! Facilis.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum
+          suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan
+          lacus vel facilisis labore et dolore magna aliqua. Quis ipsum
+          suspendisse ultrices gravida. Risus commodo viverra maecenas.
         </p>
       </div>
       <div className="bottom">
-        {data.map((item) => (
-          <Card item={item} key={item.id} />
-        ))}
+        {error
+          ? "Something went wrong!"
+          : loading
+          ? "loading"
+          : data?.map((item) => <Card item={item} key={item.id} />)}
       </div>
     </div>
   );
