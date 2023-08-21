59fddf97da600f9a572082727296d68678105866
vishaaal <officialvk02@gmail.com>
2023-08-21 06:36:20 +0530

    add alt to img

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -15,14 +15,14 @@ const Card = ({ item }) => {
             src={
               process.env.REACT_APP_UPLOAD_URL + item.attributes?.img?.data?.attributes?.url
             }
-            alt=""
+            alt="mainImg"
             className="mainImg"
           />
           <img
             src={
               process.env.REACT_APP_UPLOAD_URL + item.attributes?.img2?.data?.attributes?.url
             }
-            alt=""
+            alt="secondImg"
             className="secondImg"
           />
         </div>
