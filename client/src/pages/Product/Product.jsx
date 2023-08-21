db99afcf309527b7c5d9735f08033de157370a51
vishaaal <officialvk02@gmail.com>
2023-08-21 13:48:48 +0530

    [fetch] single product page

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -3,24 +3,27 @@ import "./Product.scss";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import BalanceIcon from "@mui/icons-material/Balance";
+import { useParams } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
 
 const Product = () => {
-  const [selectedImg, setSelectedImg] = useState(0);
+  const id = useParams().id;
+  const [selectedImg, setSelectedImg] = useState("img");
   const [quantity, setQuantity] = useState(0);
-  const images = [
-    "https://images.pexels.com/photos/10026491/pexels-photo-10026491.png?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    "https://images.pexels.com/photos/12179283/pexels-photo-12179283.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-  ];
+  const { data, loading, error } = useFetch(
+    `/products/${id}?populate=*`
+  );
 
   return (
     <div className="product">
-      <div className="left">
+
+      {loading? ("loading") : (<><div className="left">
         <div className="images">
-          <img src={images[0]} alt="" onClick={(e) => setSelectedImg(0)} />
-          <img src={images[1]} alt="" onClick={(e) => setSelectedImg(1)} />
+          <img src={process.env.REACT_APP_UPLOAD_URL +data?.attributes?.img?.data?.attributes?.url} alt="" onClick={(e) => setSelectedImg("img")} />
+          <img src={process.env.REACT_APP_UPLOAD_URL +data?.attributes?.img2?.data?.attributes?.url} alt="" onClick={(e) => setSelectedImg("img2")} />
         </div>
         <div className="mainImg">
-          <img src={images[selectedImg]} alt="" />
+          <img src={process.env.REACT_APP_UPLOAD_URL +data?.attributes[selectedImg].data?.attributes?.url} alt="" />
         </div>
       </div>
       <div className="right">
@@ -65,7 +68,7 @@ const Product = () => {
               <hr />
               <span>FAQ</span>
             </div>
-          </div>
+          </div></>)}
     </div>
   );
 };
